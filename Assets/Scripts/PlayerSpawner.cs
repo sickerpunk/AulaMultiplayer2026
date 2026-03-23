0e2043528b5b3c93fff95ad095e6d45376908fb4
sickerpunk <laura.meneghel@outlook.com>
2026-03-23 11:03:02 -0300

    alterações skybox/objetos

---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+
+public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
+{
+    public GameObject playerPrefab;
+    public void PlayerJoined(PlayerRef player)
+    {
+        if(player == Runner.LocalPlayer)
+        {
+            Runner.Spawn(playerPrefab, Vector3.zero, Quaternion.identity, player);
+        }
+    }
+}
